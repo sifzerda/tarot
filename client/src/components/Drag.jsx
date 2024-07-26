e8c98afc452bb20c2818911bad6c076148979ac3
Troy <tydamon@hotmail.com>
2024-07-26 18:53:21 +1000

    cards in drag.jsx changed to tarot cards initialcards

--- client/src/components/Drag.jsx
+++ client/src/components/Drag.jsx
@@ -3,65 +3,53 @@ import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 // Define initial squares and boxes data
 const initialCards = [
-  // HEARTS 
-  { id: 'card-1', suit: 'Hearts', rank: 'Ace' },
-  { id: 'card-2', suit: 'Hearts', rank: '2' },
-  { id: 'card-3', suit: 'Hearts', rank: '3' },
-  { id: 'card-4', suit: 'Hearts', rank: '4' },
-  { id: 'card-5', suit: 'Hearts', rank: '5' },
-  { id: 'card-6', suit: 'Hearts', rank: '6' },
-  { id: 'card-7', suit: 'Hearts', rank: '7' },
-  { id: 'card-8', suit: 'Hearts', rank: '8' },
-  { id: 'card-9', suit: 'Hearts', rank: '9' },
-  { id: 'card-10', suit: 'Hearts', rank: '10' },
-  { id: 'card-11', suit: 'Hearts', rank: 'Jack' },
-  { id: 'card-12', suit: 'Hearts', rank: 'Queen' },
-  { id: 'card-13', suit: 'Hearts', rank: 'King' },
-  
-  // DIAMONDS
-  { id: 'card-14', suit: 'Diamonds', rank: 'Ace' },
-  { id: 'card-15', suit: 'Diamonds', rank: '2' },
-  { id: 'card-16', suit: 'Diamonds', rank: '3' },
-  { id: 'card-17', suit: 'Diamonds', rank: '4' },
-  { id: 'card-18', suit: 'Diamonds', rank: '5' },
-  { id: 'card-19', suit: 'Diamonds', rank: '6' },
-  { id: 'card-20', suit: 'Diamonds', rank: '7' },
-  { id: 'card-21', suit: 'Diamonds', rank: '8' },
-  { id: 'card-22', suit: 'Diamonds', rank: '9' },
-  { id: 'card-23', suit: 'Diamonds', rank: '10' },
-  { id: 'card-24', suit: 'Diamonds', rank: 'Jack' },
-  { id: 'card-25', suit: 'Diamonds', rank: 'Queen' },
-  { id: 'card-26', suit: 'Diamonds', rank: 'King' },
-  
-  // CLUBS
-  { id: 'card-27', suit: 'Clubs', rank: 'Ace' },
-  { id: 'card-28', suit: 'Clubs', rank: '2' },
-  { id: 'card-29', suit: 'Clubs', rank: '3' },
-  { id: 'card-30', suit: 'Clubs', rank: '4' },
-  { id: 'card-31', suit: 'Clubs', rank: '5' },
-  { id: 'card-32', suit: 'Clubs', rank: '6' },
-  { id: 'card-33', suit: 'Clubs', rank: '7' },
-  { id: 'card-34', suit: 'Clubs', rank: '8' },
-  { id: 'card-35', suit: 'Clubs', rank: '9' },
-  { id: 'card-36', suit: 'Clubs', rank: '10' },
-  { id: 'card-37', suit: 'Clubs', rank: 'Jack' },
-  { id: 'card-38', suit: 'Clubs', rank: 'Queen' },
-  { id: 'card-39', suit: 'Clubs', rank: 'King' },
+  // rightside
+  { id: 'card-1', direction: 'Rightside', rank: 'The Fool' },
+  { id: 'card-2', direction: 'Rightside', rank: 'The Magician' },
+  { id: 'card-3', direction: 'Rightside', rank: 'The High Priestess' },
+  { id: 'card-4', direction: 'Rightside', rank: 'The Empress' },
+  { id: 'card-5', direction: 'Rightside', rank: 'The Emperor' },
+  { id: 'card-6', direction: 'Rightside', rank: 'The Hierophant' },
+  { id: 'card-7', direction: 'Rightside', rank: 'The Lovers' },
+  { id: 'card-8', direction: 'Rightside', rank: 'The Chariot' },
+  { id: 'card-9', direction: 'Rightside', rank: 'Justice' },
+  { id: 'card-10', direction: 'Rightside', rank: 'The Hermit' },
+  { id: 'card-11', direction: 'Rightside', rank: 'Wheel Of Fortune' },
+  { id: 'card-12', direction: 'Rightside', rank: 'Strength' },
+  { id: 'card-13', direction: 'Rightside', rank: 'The Hanged Man' },
+  { id: 'card-14', direction: 'Rightside', rank: 'Death' },
+  { id: 'card-15', direction: 'Rightside', rank: 'Temperance' },
+  { id: 'card-16', direction: 'Rightside', rank: 'The Devil' },
+  { id: 'card-17', direction: 'Rightside', rank: 'The Tower' },
+  { id: 'card-18', direction: 'Rightside', rank: 'The Star' },
+  { id: 'card-19', direction: 'Rightside', rank: 'The Moon' },
+  { id: 'card-20', direction: 'Rightside', rank: 'The Sun' },
+  { id: 'card-21', direction: 'Rightside', rank: 'Judgement' },
+  { id: 'card-22', direction: 'Rightside', rank: 'The World' },
+  // inverted
+  { id: 'card-23', suit: 'Inverted', rank: '10' },
+  { id: 'card-24', suit: 'Inverted', rank: 'Jack' },
+  { id: 'card-25', suit: 'Inverted', rank: 'Queen' },
+  { id: 'card-26', suit: 'Inverted', rank: 'King' },
+  { id: 'card-27', suit: 'Inverted', rank: 'Ace' },
+  { id: 'card-28', suit: 'Inverted', rank: '2' },
+  { id: 'card-29', suit: 'Inverted', rank: '3' },
+  { id: 'card-30', suit: 'Inverted', rank: '4' },
+  { id: 'card-31', suit: 'Inverted', rank: '5' },
+  { id: 'card-32', suit: 'Inverted', rank: '6' },
+  { id: 'card-33', suit: 'Inverted', rank: '7' },
+  { id: 'card-34', suit: 'Inverted', rank: '8' },
+  { id: 'card-35', suit: 'Inverted', rank: '9' },
+  { id: 'card-36', suit: 'Inverted', rank: '10' },
+  { id: 'card-37', suit: 'Inverted', rank: 'Jack' },
+  { id: 'card-38', suit: 'Inverted', rank: 'Queen' },
+  { id: 'card-39', suit: 'Inverted', rank: 'King' },
+  { id: 'card-40', suit: 'Inverted', rank: 'Ace' },
+  { id: 'card-41', suit: 'Inverted', rank: '2' },
+  { id: 'card-42', suit: 'Inverted', rank: '3' },
+  { id: 'card-43', suit: 'Inverted', rank: '4' },
+  { id: 'card-44', suit: 'Inverted', rank: '5' },
 
-  // SPADES
-  { id: 'card-40', suit: 'Spades', rank: 'Ace' },
-  { id: 'card-41', suit: 'Spades', rank: '2' },
-  { id: 'card-42', suit: 'Spades', rank: '3' },
-  { id: 'card-43', suit: 'Spades', rank: '4' },
-  { id: 'card-44', suit: 'Spades', rank: '5' },
-  { id: 'card-45', suit: 'Spades', rank: '6' },
-  { id: 'card-46', suit: 'Spades', rank: '7' },
-  { id: 'card-47', suit: 'Spades', rank: '8' },
-  { id: 'card-48', suit: 'Spades', rank: '9' },
-  { id: 'card-49', suit: 'Spades', rank: '10' },
-  { id: 'card-50', suit: 'Spades', rank: 'Jack' },
-  { id: 'card-51', suit: 'Spades', rank: 'Queen' },
-  { id: 'card-52', suit: 'Spades', rank: 'King' },
 ];
 
 const initialDecks = [
@@ -127,7 +115,7 @@ const Drag = () => {
                         {...provided.dragHandleProps}
                         ref={provided.innerRef}
                       >
-                        {cards[currentCardIndex].rank} of {cards[currentCardIndex].suit}
+                        {cards[currentCardIndex].rank}
                       </div>
                     )}
                   </Draggable>
@@ -151,7 +139,7 @@ const Drag = () => {
                     >
                       {deck.cards.map((card, index) => (
                         <div key={card.id} className="card-in-deck">
-                          {card.rank} of {card.suit}
+                          {card.rank}
                         </div>
                       ))}
                       {provided.placeholder}
